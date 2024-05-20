39926c463c5f9b744b59ef76641e21df61073d8d
SirWilliamsthefirst <odinakaifembonu@gmail.com>
2024-05-20 13:41:16 +0100

    (week 10 commit)

--- practice/P1.py
+++ practice/P1.py
@@ -1,1 +1,15 @@
-print("Python Frist Program...")
+score = input("Enter Score: ")
+scr = float(score)
+if 0.0<scr<1.0:
+    if scr >= 0.9:
+        print("A")
+    elif scr >= 0.8:
+        print("B")
+    elif scr >= 0.7:
+        print("C")
+    elif scr >= 0.6:
+        print("D")
+    elif scr < 0.6:
+        print("F")
+else:
+    print("Error")
